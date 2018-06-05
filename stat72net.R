ac5465cc14020d4595dbc885072229956130e27d
Sankalp Gupta <31817289+sankalpsg@users.noreply.github.com>
2018-06-05 18:42:39 +0100

    Add files via upload

--- stat72net.R
+++ stat72net.R
@@ -1,9 +1,9 @@
 
-net2016 <- read.csv("C:/Users/Sankalp/Desktop/DataScience Trinity/Dissertation/Mimi Zhang Files/CitiBike/CitiBike/72weight2016.csv")
-net2017 <- read.csv("C:/Users/Sankalp/Desktop/DataScience Trinity/Dissertation/Mimi Zhang Files/CitiBike/CitiBike/72weight2017.csv")
-net2018 <- read.csv("C:/Users/Sankalp/Desktop/DataScience Trinity/Dissertation/Mimi Zhang Files/CitiBike/CitiBike/72weight2018.csv")
+# net2016 <- read.csv("C:/Users/Sankalp/Desktop/DataScience Trinity/Dissertation/Mimi Zhang Files/CitiBike/CitiBike/72weight2016.csv")
+# net2017 <- read.csv("C:/Users/Sankalp/Desktop/DataScience Trinity/Dissertation/Mimi Zhang Files/CitiBike/CitiBike/72weight2017.csv")
+# net2018 <- read.csv("C:/Users/Sankalp/Desktop/DataScience Trinity/Dissertation/Mimi Zhang Files/CitiBike/CitiBike/72weight2018.csv")
 
 
-st72net  <- rbind.fill(net2016,net2017,net2018)
+stnet  <- rbind.fill(weight2016,weight2017,weight2018)
 
-write.csv(st72net, "stat72net.csv", row.names = FALSE)
+write.csv(stnet, "statnet.csv", row.names = FALSE)
