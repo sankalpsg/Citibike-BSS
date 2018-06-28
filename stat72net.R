f4bb40072480a422354e7e51d443c5b609176b82
Sankalp Gupta <31817289+sankalpsg@users.noreply.github.com>
2018-06-28 04:55:55 +0100

    Add files via upload
    Updated

--- stat72net.R
+++ stat72net.R
@@ -6,4 +6,44 @@
 
 stnet  <- rbind.fill(weight2016,weight2017,weight2018)
 
-write.csv(stnet, "statnet.csv", row.names = FALSE)
+library(dplyr)
+finalNet <- rename(stnet, date = dat2,
+            station_id = a,
+            Netflow_TZ1 = X1,
+            Netflow_TZ2 = X1.1,
+            Netflow_TZ3 = X1.2,
+            Netflow_TZ4 = X1.3,
+            Netflow_TZ5 = X1.4,
+            Netflow_TZ6 = X1.5,
+            Netflow_TZ7 = X1.6,
+            Netflow_TZ8 = X1.7)
+# 
+# write.csv(finalNet, "statnet.csv", row.names = FALSE)
+# saveRDS(finalNet, file = "finaldata.rds")
+
+# finalNet <- rename(finalNet, a = station_id)
+finalNet$a <- as.integer(finalNet$a)
+finalStation <- merge(x = finalNet, y = station)
+
+finalStation$station_id <- as.factor(finalStation$station_id)
+finalStation <- finalStation[,c(2:5,14,6:13)]
+
+write.csv(finalStation, "statnet.csv", row.names = FALSE)
+saveRDS(finalStation, file = "finaldata.rds")
+
+
+###########################################################
+
+finalStatn  <- readRDS(file = "C:/Users/Sankalp/Desktop/DataScience Trinity/Dissertation/Mimi Zhang Files/CitiBike/CitiBike/finaldata.rds")
+
+
+wei <- finalStatn
+w1 <- wei[wei$date >= "2018-03-01" & wei$date <= "2018-03-31",]
+
+
+store <- apply(dist,1 ,function(x) which(x==1))
+head(store)
+
+store <- as.data.frame(store)
+
+
